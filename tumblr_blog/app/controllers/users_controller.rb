79082cccf3657cda56e1303f7cc254646bc69d17
Thomas Thornton <thomas.thornton@stonybrook.edu>
2014-07-02 16:14:58 -0400

    Can create users, and list them in the index view

--- tumblr_blog/app/controllers/users_controller.rb
+++ tumblr_blog/app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user
+      flash[:notice] = "New User Created!"
+      redirect_to users_path
+    else
+      flash[:alert] = "There was a problem."
+      redirect_to new_user_path
+    end
   end
 
 
@@ -21,4 +31,10 @@ class UsersController < ApplicationController
   def destroy
   end
   
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :password, :bio)
 end
