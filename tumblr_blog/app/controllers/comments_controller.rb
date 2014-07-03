d712c02349fdb0e2e3858b56f3d95c34c8a7b970
Melissa Lin <melissavlin@gmail.com>
2014-07-03 15:36:32 -0400

    created comment form

--- tumblr_blog/app/controllers/comments_controller.rb
+++ tumblr_blog/app/controllers/comments_controller.rb
@@ -1,8 +1,22 @@
 class CommentsController < ApplicationController
+
+
   def index
   end
 
   def new
+  end
+
+  def create
+  	@comment = Comment.new(comment_params)
+  	@comment.user = User.find(session[:user_id])
+  	if @comment.save
+  		flash[:notice] = "Thank you for your comment."
+  		redirect_to post_path
+  	else
+  		flash[:alert] = "There was a problem posting your comment."
+  		redirect_to post_path
+  	end
   end
 
   def show
@@ -10,4 +24,11 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+private
+def comment_params
+	params.require(:comment).permit(:body)
+end
+
+
 end
